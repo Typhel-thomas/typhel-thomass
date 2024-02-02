8616dd79b2440d8abb5006789578f366d98f525f
Typhel-thomas <158449462+Typhel-thomas@users.noreply.github.com>
2024-02-02 11:44:19 +0530

    Add files via upload

---
+++ src/App.js
@@ -0,0 +1,53 @@
+import logo from './logo.svg';
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+     <div>
+      <div className="lay1">
+        <h1> 9:41</h1>
+      </div>
+      <div>
+        <div className="lay2">
+          <h1>hello</h1>
+          <p>hi james</p>
+        </div>
+        <div>
+          <div className="lay3">
+            <div>
+              <div className="lay4">
+                <h1>Dr Imran Shahir</h1>
+                <p>General director</p>
+                <p>sunday,12 june</p>
+              </div>
+              <div>
+                <div className="lay5">
+                  <p>search doctors or health issue</p>
+                </div>
+                <div className="lay6">
+                  <p>covid 19</p>
+                </div>
+                <div>
+                  <div className="lay7">
+                    <h1>Dear Doctor</h1>
+                    <div>
+                      <div className="lay8">
+                        <h1>Dr Joseph brostito</h1>
+                        <p>Dental specialist</p>
+                      </div>
+                    </div>
+                  </div>
+                </div>
+              </div>
+            </div>
+          </div>
+        </div>
+      </div>
+      </div> 
+        
+    </div>
+  );
+}
+
+export default App;
